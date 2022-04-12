13a51ed583df79e0f7480e31528321c71e845590
Melisawidiawati <90242961+Melisawidiawati@users.noreply.github.com>
2022-04-12 22:54:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send(`
+        Nama saya Melisa Widiawati <br/>
+        NIM: 51019015 <br/>
+        Jurusan Sistem Informasi <br/>
+        Mata Kuliah Cloud Computing!
+    `)
 })
 
 app.listen(PORT, HOST)
